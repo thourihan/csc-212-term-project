3b232bcfec9321e9e3e5cf32e72a7e1d9f45bf8b
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-11-21 12:35:16 -0500

    Update FractalMaker.cpp

--- FractalMaker.cpp
+++ FractalMaker.cpp
@@ -1,1 +1,13 @@
 #include "FractalMaker.h"
+
+void FractalMaker::kochSnowflake(int numRecursions) {
+    cout << "Generating a koch snowflake with " << numRecursions << " recursions..." << endl;
+}
+
+void FractalMaker::hilbertCurve(int numRecursions) {
+    cout << "Generating a hilbert curve with " << numRecursions << " recursions..." << endl;
+}
+
+void FractalMaker::sierpinskiTriangle(int numRecursions) {
+    cout << "Generating a sierpinski triangle with " << numRecursions << " recursions..." << endl;
+}
