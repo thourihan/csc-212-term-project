aad725b34f8ffece20ee0d2238d5d2e712c7f73e
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-11-21 12:02:32 -0500

    Add files via upload

---
+++ FractalMaker.cpp
@@ -0,0 +1,1 @@
+#include "FractalMaker.h"
---
+++ FractalMaker.h
@@ -0,0 +1,12 @@
+#ifndef CSC212_TERM_PROJECT_FRACTALMAKER_H
+#define CSC212_TERM_PROJECT_FRACTALMAKER_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class FractalMaker {
+
+};
+
+
+#endif
