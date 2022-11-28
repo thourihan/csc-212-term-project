102004a39dda92710553ec9c7fce1775fdf1e8de
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-11-28 18:08:39 -0500

    Add files via upload

---
+++ CSC212_term_project/FractalMaker.h
@@ -0,0 +1,15 @@
+#ifndef CSC212_TERM_PROJECT_FRACTALMAKER_H
+#define CSC212_TERM_PROJECT_FRACTALMAKER_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class FractalMaker {
+    public:
+        void sierpinskiTriangle(int numRecursions);
+        void hilbertCurve(int numRecursions);
+        void kochSnowflake(int numRecursions);
+};
+
+
+#endif
