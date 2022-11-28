46796467458cb0507937bad15c01d250f698985c
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-11-28 18:39:55 -0500

    Update main.cpp

--- CSC212_term_project/main.cpp
+++ CSC212_term_project/main.cpp
@@ -6,7 +6,6 @@ char getFractal();
 int getNumRecursions();
 
 int main() {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
     char fractal = getFractal();
     int numRecursions = getNumRecursions();
 
@@ -20,6 +19,19 @@ int main() {
         fractalMaker.kochSnowflake(numRecursions);
     }
 
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 }
 
 // Asks user to indicate which fractal they would like to produce.
