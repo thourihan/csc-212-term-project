da5a7301cc313647a61f248482d7847dde46e8e6
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-11-29 11:52:50 -0500

    Update FractalMaker.h

--- CSC212_term_project/FractalMaker.h
+++ CSC212_term_project/FractalMaker.h
@@ -12,11 +12,10 @@ public:
     void hilbertCurve(int numRecursions);
     void kochSnowflake(int numRecursions);
 private:
-    void drawTriangle(int &top, int &left, int &right, sf::RenderWindow &window);
+    void drawTriangle(pair<float, float> &left, pair<float, float> &top, pair<float, float> &right, sf::RenderWindow &window);
     void sierpinskiHelper(int numRecursions);
     void hilbertHelper(int numRecursions);
     void kochHelper(int numRecursions);
-    void drawTriangle(pair<float, float> &top, pair<float, float> &left, pair<float, float> &right, sf::RenderWindow &window);
 };
 
 #endif
