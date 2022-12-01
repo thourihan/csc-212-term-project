3c644e85e014f68e3e626bb33e78c9bd428dec97
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-12-01 13:02:16 -0500

    Update FractalMaker.h

--- CSC212_term_project/FractalMaker.h
+++ CSC212_term_project/FractalMaker.h
@@ -16,7 +16,7 @@ public:
 
 private:
     void drawTriangle(sf::Color color, bool upsideDown, float scale, float xPos, float yPos);
-    void kDrawTriangle(sf::Color color, float rotate, float scale, float xPos, float yPos);
+    void kDrawTriangle(sf::Color color, float rotation, float scale, float xPos, float yPos);
 
     void sierpinskiHelper(int numRecursions, float scale, float xPos, float yPos);
     void hilbertHelper(int numRecursions);
