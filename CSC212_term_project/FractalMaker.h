4c0cc987db584782ff4aea5c3e434cbdfe7d1195
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-12-01 12:36:40 -0500

    Update FractalMaker.h

--- CSC212_term_project/FractalMaker.h
+++ CSC212_term_project/FractalMaker.h
@@ -16,7 +16,8 @@ public:
     int maxRecursions;
 
 private:
-    void drawTriangle(sf::Color color, bool upsideDown, bool leftR, bool rightR,bool kochSnowflake, float scale, float xPos, float yPos);
+    void drawTriangle(sf::Color color, bool upsideDown, float scale, float xPos, float yPos);
+    void kDrawTriangle(sf::Color color, bool upsideDown, bool leftR, bool rightR, bool kochSnowflake,float scale, float xpos, float ypos);
     void sierpinskiHelper(int numRecursions, float scale, float xPos, float yPos);
     void hilbertHelper(int numRecursions);
     void kochHelper(int numRecursions);
